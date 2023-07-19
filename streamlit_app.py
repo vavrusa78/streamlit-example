88bd95bf88ac25ceb74b40ba7b6d6c051c388e28
Pavel Vavrinek <108886693+vavrusa78@users.noreply.github.com>
2023-07-19 11:07:10 +0200

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,54 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-"""
-# Welcome to Streamlit!
+# Define a function that calculates the cashback data
+def calculate_cashback(num_employees, avg_monthly_users_percentage, avg_volume_per_employee, cashback_percentage=2):
+    cashback_percentage /= 100
+    avg_monthly_users_percentage /= 100
+    months = np.arange(1, 13)
+    active_users_percentage = np.where(months <= 6, months / 6 * avg_monthly_users_percentage, avg_monthly_users_percentage)
+    monthly_active_users = num_employees * active_users_percentage
+    individual_monthly_cashback = avg_volume_per_employee * cashback_percentage
+    total_monthly_cashback = individual_monthly_cashback * monthly_active_users
+    individual_cumulative_cashback = np.cumsum(np.repeat(individual_monthly_cashback, 12))
+    total_cumulative_cashback = np.cumsum(total_monthly_cashback)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+    cashback_data = pd.DataFrame({
+        'Month': months,
+        'Active Users': monthly_active_users,
+        'Individual Monthly Cashback': individual_monthly_cashback,
+        'Total Monthly Cashback': total_monthly_cashback,
+        'Individual Cumulative Cashback': individual_cumulative_cashback,
+        'Total Cumulative Cashback': total_cumulative_cashback
+    })
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    return cashback_data
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Use Streamlit to create the web app
+st.title('Cashback Calculator')
 
+# Create input fields for the user to enter their data
+num_employees = st.number_input('Enter the total number of employees', value=1000)
+avg_monthly_users_percentage = st.number_input('Enter the average percentage of active users after 6 months', value=40)
+avg_volume_per_employee = st.number_input('Enter the average expenditure per individual', value=5000)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# Call the function to calculate the cashback data
+cashback_data = calculate_cashback(num_employees, avg_monthly_users_percentage, avg_volume_per_employee)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# Display the data in a table
+st.dataframe(cashback_data)
 
-    points_per_turn = total_points / num_turns
+# Create a plot of the individual and total cumulative cashback
+fig, ax = plt.subplots()
+ax.plot(cashback_data['Month'], cashback_data['Individual Cumulative Cashback'], label='Individual Cumulative Cashback')
+ax.plot(cashback_data['Month'], cashback_data['Total Cumulative Cashback'], label='Total Cumulative Cashback')
+ax.set_xlabel('Month')
+ax.set_ylabel('Cumulative Cashback')
+ax.set_title('Cumulative Cashback over One Year')
+ax.legend()
+ax.grid(True)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Display the plot
+st.pyplot(fig)
